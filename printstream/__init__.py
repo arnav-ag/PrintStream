045167f4ba18abe89595ab45da09fc6126564498
arnav-ag <arnav.ctf@gmail.com>
2023-11-03 00:30:48 +0800

    Fix configure

--- printstream/__init__.py
+++ printstream/__init__.py
@@ -1,2 +1,2 @@
-from .config import set_format, set_level, set_output, set_align, set_repeat_func_name, set_colorize
+from .config import set_format, set_level, set_output, set_align, set_repeat_func_name, set_colorize, configure
 from .activate import activate, deactivate
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='printstream',
-    version='0.1.1',
+    version='0.1.2',
     author='Arnav',
     author_email='arnavaggarwalwork@gmail.com',
     description='A library for enhanced print debugging.',
